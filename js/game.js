cbef9cf25e5e7ed4cec6ac566c2af3587d76a768
eddesks <ericdenniso@gmail.com>
2024-02-02 11:38:35 -0300

    modificado game.css, game.html / criado arquivo game.js

---
+++ js/game.js
@@ -0,0 +1,51 @@
+const grid = document.querySelector('.grid');
+
+const characteres = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+
+
+]
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+
+}
+
+const createCard = (character) =>{
+
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('/images/${character}.png')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card
+
+
+}
+
+const loadGame = () => {
+    characteres.forEach((character) => {
+
+        const card = createCard(character);
+        grid.appendChild(card)
+
+    });
+}
+
+loadGame()
